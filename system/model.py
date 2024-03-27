5ea9ee43f4029d6e4f82a29c0aaeefbadd773763
Repcak2000 <276940@student.pwr.edu.pl>
2024-03-27 10:13:54 +0100

    Saving and loading of system added.

--- system/model.py
+++ system/model.py
@@ -4,6 +4,10 @@ from PIL import Image
 import os
 import face_recognition
 from sklearn import svm
+import pickle
+from omegaconf import OmegaConf
+from typing import Optional
+import yaml
 
 
 class Model:
@@ -11,10 +15,25 @@ class Model:
     Model for embedding retrieval. Wrapper for face_recognition
     """
 
-    def __init__(self):
-        self.encodings = []
-        self.names = []
-        self.clf_svm = svm.SVC()
+    def __init__(self, model_config_path: Optional[str] = None):
+        """
+        Load model from pickle files or create new model.
+        :param model_config_path: Path to model configuration with encodings_path, names_path, svm_pickle_path.
+        """
+        if model_config_path:
+            self.config = OmegaConf.load(model_config_path)
+            encodings_path = self.config.get("encodings_path")
+            names_path = self.config.get("names_path")
+            svm_path = self.config.get("svm_pickle_path")
+
+            self.encodings = pickle.load(open(encodings_path, "rb"))
+            self.names = pickle.load(open(names_path, "rb"))
+            self.clf_svm = pickle.load(open(svm_path, "rb"))
+
+        else:
+            self.encodings = []
+            self.names = []
+            self.clf_svm = svm.SVC()
 
     def _image_preprocessing(self, image: Image):
         # TODO: I dont know if this is even needed.
@@ -51,6 +70,26 @@ class Model:
                         + " was skipped and can't be used for training"
                     )
 
+    def save_system(self, save_dir: str, system_configuration_name: str = "system"):
+        if not os.path.exists(save_dir):
+            os.makedirs(save_dir)
+            
+        paths_set = {
+            "svm_pickle_path": os.path.join(save_dir, "svm.pkl"),
+            "encodings_path": os.path.join(save_dir, "encodings.pkl"),
+            "names_path": os.path.join(save_dir, "names.pkl"),
+        }
+        with open(os.path.join(save_dir, "svm.pkl"), "wb") as file:
+            pickle.dump(self.clf_svm, file)
+        with open(os.path.join(save_dir, "encodings.pkl"), "wb") as file:
+            pickle.dump(self.encodings, file)
+        with open(os.path.join(save_dir, "names.pkl"), "wb") as file:
+            pickle.dump(self.names, file)
+        print(f"System saved to directory: {save_dir}")
+        with open(f"config/{system_configuration_name}.yaml", "w") as config_file:
+            yaml.dump(paths_set, config_file, sort_keys=True)
+        print(f"Config file created and saved, filename: config/{system_configuration_name}.yaml")
+
     def fit_svm(
         self,
         train_dir_path: str,
@@ -59,19 +98,28 @@ class Model:
         degree=3,
         gamma="scale",
         coef0=0.0,
+        verbose=True,
     ) -> svm.SVC:
 
         self._train_embeddings_names(train_dir_path)
 
         self.clf_svm = svm.SVC(
-            C=C, kernel=kernel, degree=degree, gamma=gamma, coef0=coef0
+            C=C, kernel=kernel, degree=degree, gamma=gamma, coef0=coef0, verbose=verbose
         )
         self.clf_svm.fit(self.encodings, self.names)
 
         return self.clf_svm
 
-    def add_user(self, image: Image):
-        return NotImplementedError
+    def add_user(self, name: str, image_path: str):
+        """
+        Method to add user.
+        :param name: Name of the user
+        :param image_path: Path to the User's image.
+        """
+        img = face_recognition.load_image_file(image_path)
+        face_enc = face_recognition.face_encodings(img)[0]
+        self.encodings.append(face_enc)
+        self.names.append(name)
 
     def verify_user(self, image_path: str):
         """
